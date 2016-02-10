f3d463dcb4d25ad2745e37dad608f04a3717e2ce
Viktor Eikman <viktor.eikman@gmail.com>
2016-02-10 08:25:17 +0100

    0.9.0 release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='CBG',
-    version='0.9.0~alpha1',
+    version='0.9.0',
     description='Card-based game creation library',
     requires=['yaml', 'numpy', 'lxml'],
     author='Viktor Eikman',
